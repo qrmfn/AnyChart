51f8e52032d9b69067f5eed2f73bfb0473c9681e
chernetsky <chernetsky@gmail.com>
2018-09-03 10:56:37 +0800

    lf anchor center

--- src/core/defaultTheme.js
+++ src/core/defaultTheme.js
@@ -818,7 +818,7 @@ goog.exportSymbol('anychart.themes.defaultTheme', {
       'width': false,
       'height': false
     },
-    // 'anchor': 'center',
+    'anchor': 'center',
     'padding': 4,
     'rotation': 0,
     'format': anychart.core.defaultTheme.returnValue,
